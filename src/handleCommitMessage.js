b94053e63365521d5c24304b9ef2a8356fd6b0ad
Ofer Segev <ofs@google.com>
2019-12-13 13:29:27 +0200

    Support users that do not use git. Console.log the commit message

--- src/handleCommitMessage.js
+++ src/handleCommitMessage.js
@@ -20,6 +20,11 @@ const path = require('path');
 
 module.exports = function handleCommitMessage(commitMessage) {
   const CWD = process.cwd();
-  fs.writeFileSync(path.join(CWD, '.git', 'COMMIT_EDITMSG'), commitMessage);
-  console.log(green(`Run "git commit -F .git/COMMIT_EDITMSG -e"`));
+  try {
+    fs.writeFileSync(path.join(CWD, '.git', 'COMMIT_EDITMSG'), commitMessage);
+    console.log(green(`Run "git commit -F .git/COMMIT_EDITMSG -e"`));
+  } catch (e) {
+    // Project is not using git
+    console.log(commitMessage);
+  }
 };
